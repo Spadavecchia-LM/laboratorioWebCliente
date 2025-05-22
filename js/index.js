3ac9867aac3088c9c79baacfd26e91f0c6e7b4e3
Spadavecchia-LM <leonardo.spadavecchia@hotmail.com>
2025-05-22 20:09:35 -0300

    primer prototipo del sitio, pagina de inicio y contenedor de productos en la pagina products

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ import { fetchProducts } from './data.js';
 
 const cardsContainer = document.querySelector("#card_container");
 
+//hecho por leo
+const carrouselContainer = document.querySelector('#carousel-inner');
+
+
 let currentProduct = null; // para saber qué producto se está viendo
 
 document.addEventListener("click", (e) => {
@@ -27,6 +31,7 @@ document.addEventListener("click", (e) => {
   }
 });
 
+//se modifico el estilo de la card para que quede bien en la pagina (leo)
 async function renderProducts() {
   const products = await fetchProducts();
 
@@ -34,8 +39,12 @@ async function renderProducts() {
 
   products.forEach(product => {
     const card = document.createElement("div");
-    card.style.width = "18rem";
-    card.classList.add("card", "m-2", "card-hover", "position-relative");
+
+    card.className = `
+  card custom-card
+  p-2 col-6 col-md-3 col-lg-2
+  d-flex flex-column align-items-center card-hover
+`;
     card.setAttribute("data-id", product.id);
 
     card.innerHTML = `
@@ -51,6 +60,50 @@ async function renderProducts() {
 
     cardsContainer.appendChild(card);
   });
+
 }
 
 renderProducts();
+
+//hecho por leo
+async function renderCarrousel() {
+
+  const productos = await fetchProducts();
+
+  carrouselContainer.innerHTML = ''
+
+  // agrupar de a 3 productos por slide
+  for (let i = 0; i < productos.length; i += 3) {
+    const grupo = productos.slice(i, i + 3);
+
+    const slide = document.createElement('div');
+    slide.className = 'carousel-item' + (i === 0 ? ' active' : '');
+
+    const row = document.createElement('div');
+    row.className = 'row justify-content-center';
+
+
+    grupo.forEach(producto => {
+      const col = document.createElement('div');
+      col.className = 'col-md-4 d-flex justify-content-center';
+
+
+      col.innerHTML = `
+            <div class="card">
+              <img src="${producto.image}" class="card-img-top product-img" alt="${producto.title}">
+              <div class="card-body">
+                <h5 class="card-title">${producto.title}</h5>
+                <p class="card-text">$${producto.price}</p>
+              </div>
+            </div>
+          `;
+
+      row.appendChild(col);
+    });
+
+    slide.appendChild(row);
+    carrouselContainer.appendChild(slide);
+  }
+}
+//hecho por leo
+renderCarrousel();
